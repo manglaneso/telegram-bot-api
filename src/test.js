9447e8be84938ba804142b94b024bc5631b650fd
manglo <andresmanglano@protonmail.com>
2024-01-29 12:56:19 +0100

    remove test file

--- src/test.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-  let c = client("185711772:AAGSL0RQhSPn2BRCXQMEX7xhKteQXSixTeI");
-  c.sendMessage({chatId: "-23232799", text: "haisds", replyParameters: {'message_id': 482}})
-}
